78f79bb4f55c9249504ef45112f084a044ccb429
Hardys <hardys@example.com>
2021-06-08 01:19:39 +0800

    first commit

---
+++ avod/__init__.py
@@ -0,0 +1,12 @@
+from npu_bridge.npu_init import *
+import os
+
+
+def root_dir():
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+def top_dir():
+    avod_root_dir = root_dir()
+    return os.path.split(avod_root_dir)[0]
+
---
+++ avod/tests/__init__.py
@@ -0,0 +1,7 @@
+from npu_bridge.npu_init import *
+import os
+
+
+def test_path():
+    return os.path.dirname(os.path.realpath(__file__))
+
